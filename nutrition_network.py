8c8634ffa55afd3f8f1ed5af20254fffa3892ba6
Ruben Pierich <rpierich@hotmail.com>
2019-09-25 22:25:23 -0400

    Pulled in dataframes from nutriebnt text files.

---
+++ nutrition_network.py
@@ -0,0 +1,16 @@
+# Load the Pandas libraries with alias 'pd'
+import pandas as pd
+# Read data from file 'filename.csv'
+# (in the same directory that your python process is based)
+# Control delimiters, rows, column names with read_csv (see later)
+data = pd.read_csv("NUT_DATA.txt", sep='^', header=None)
+# Preview the first 5 lines of the loaded data
+data = data.rename(columns={0: 'NDB_No', 1: 'Nutr_No', 2: 'Nutr_Val', 3: 'Num_Data_Pts', 4: 'Std_Error', 5: 'Src_Cd', 6: 'Deriv_Cd', 7: 'Ref_NDB_No', 8: 'Add_Nutr_Mark', 9: 'Num_Studies', 10: 'Min', 11: 'Max', 12: 'DF', 13: 'Low_EB', 14: 'Up_EB', 15: 'Stat_cmt', 16: 'AddMod_Date', 17: 'CC'})  #  noqa
+
+print(data)
+
+nut_def_data = pd.read_csv("NUTR_DEF.txt", sep='^', header = None,  encoding  =  "ISO-8859-1")  # noqa
+# Preview the first 5 lines of the loaded data
+nut_def_data = nut_def_data.rename(columns={0: 'Nutr_No', 1: 'Units', 2: 'Tagname', 3: 'NutrDesc', 4: 'Num_Dec', 5: 'SR_Order'}, errors="raise")  # noqa
+#  usecols=['Nutr_No', 'Units', 'Tagname', 'NutrDesc', 'Num_Dec', 'SR_Order'],
+print(nut_def_data)
