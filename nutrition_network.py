db15b494bf5832a998a2c605617936046505febb
Ruben Pierich <rpierich@hotmail.com>
2019-09-25 23:02:27 -0400

    Read in food description file and named the columns

--- nutrition_network.py
+++ nutrition_network.py
@@ -14,3 +14,9 @@ nut_def_data = pd.read_csv("NUTR_DEF.txt", sep='^', header = None,  encoding  = 
 nut_def_data = nut_def_data.rename(columns={0: 'Nutr_No', 1: 'Units', 2: 'Tagname', 3: 'NutrDesc', 4: 'Num_Dec', 5: 'SR_Order'}, errors="raise")  # noqa
 #  usecols=['Nutr_No', 'Units', 'Tagname', 'NutrDesc', 'Num_Dec', 'SR_Order'],
 print(nut_def_data)
+
+food_des_data = pd.read_csv("FOOD_DES.txt", sep='^', header = None,  encoding  =  "ISO-8859-1")  # noqa
+# Preview the first 5 lines of the loaded data
+food_des_data = food_des_data.rename(columns={0: 'NDB_No', 1: 'FdGrp_Cd', 2: 'Long_Desc', 3: 'Shrt_Desc', 4: 'ComName', 5: 'ManufacName', 6: 'Survey', 7: 'Ref_desc', 8: 'Refuse', 9: 'SciName', 10: 'N_Factor', 11: 'Pro_Factor', 12: 'Fat_Factor', 13:'CHO_Factor'}, errors="raise")  # noqa
+#  usecols=['Nutr_No', 'Units', 'Tagname', 'NutrDesc', 'Num_Dec', 'SR_Order'],
+print(food_des_data)
